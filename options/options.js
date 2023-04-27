407cb9cb67268a03d5d8d6423eede841ed8022c8
makibishu <makibishu39@gmail.com>
2023-04-27 12:16:40 +0900

    年代絞り込みの機能改善

--- options/options.js
+++ options/options.js
@@ -3,26 +3,36 @@ function initialize() {
     chrome.storage.local.get(
         ['query', 'border', 'dateStart', 'dateEnd', 'sortType', 'mode']
     ).then((locals) => {
-            document.getElementById('query').value = locals.query;
-            document.getElementById('border').value = locals.border;
-            document.getElementById('dateStart').value = locals.dateStart;
-            document.getElementById('dateEnd').value = locals.dateEnd;
+        const today = new Date().toLocaleString(
+            "ja-JP",
+            {year: "numeric", month: "2-digit", day: "2-digit"}
+        ).split("/").join("-");
 
-            for (let elm of document.getElementsByName('sortType')) {
-                if (elm.value === locals.sortType) {
-                    elm.checked = true;
-                }
+        document.getElementById('query').value = locals.query;
+        document.getElementById('border').value = locals.border;
+
+        document.getElementById('dateStart').value = locals.dateStart;
+        document.getElementById('dateStart').min = '2007-03-06';
+        document.getElementById('dateStart').max = today;
+
+        document.getElementById('dateEnd').value = locals.dateEnd;
+        document.getElementById('dateEnd').min = '2007-03-06';
+        document.getElementById('dateEnd').max = today;
+
+        for (let elm of document.getElementsByName('sortType')) {
+            if (elm.value === locals.sortType) {
+                elm.checked = true;
             }
-
-            for (let elm of document.getElementsByName('mode')) {
-                if (elm.id === locals.mode) {
-                    elm.checked = true;
-                }
-            }
-
-            document.getElementById('saveButton').onclick = save;
         }
-    )
+
+        for (let elm of document.getElementsByName('mode')) {
+            if (elm.id === locals.mode) {
+                elm.checked = true;
+            }
+        }
+
+        document.getElementById('saveButton').onclick = save;
+    })
 }
 
 function save(){
